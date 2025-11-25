547fa34702bed95ea3a9e10f7dc915ee0e6d8182
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-11-25 19:26:55 +0000

    11

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function taxable(state, taxExempt) {
+  if (state === "IA" && taxExempt === false) {
+    return "is taxable";
+  }
+}
 
+
+function isVehicle(hasWheels, canFly, canSwim) {
+  if (hasWheels || canFly || canSwim) {
+    return "is vehicle";
+  }
+}
